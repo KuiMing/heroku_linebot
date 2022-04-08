56e23af19b588ed83115c743e751debee62d2cb6
KuiMing <benjamin0901@gmail.com>
2022-04-08 08:14:31 +0800

    only hello

--- application.py
+++ application.py
@@ -1,20 +1,6 @@
-import os
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import (
-    MessageEvent,
-    TextMessage,
-    TextSendMessage,
-    FlexSendMessage,
-    ImageMessage,
-)
 
 app = Flask(__name__)
-LINE_SECRET = os.getenv('LINE_SECRET')
-LINE_TOKEN = os.getenv('LINE_TOKEN')
-LINE_BOT = LineBotApi(LINE_TOKEN)
-HANDLER = WebhookHandler(LINE_SECRET)
 
 
 @app.route("/")
@@ -23,21 +9,5 @@ def hello():
     return "Hello World!!!!!"
 
 
-@app.route("/callback", methods=["POST"])
-def callback():
-    """
-    LINE bot webhook callback
-    """
-    # get X-Line-Signature header value
-    signature = request.headers["X-Line-Signature"]
-    print(signature)
-    body = request.get_data(as_text=True)
-    print(body)
-    try:
-        HANDLER.handle(body, signature)
-    except InvalidSignatureError:
-        print(
-            "Invalid signature. Please check your channel access token/channel secret."
-        )
-        abort(400)
-    return "OK"
+if __name__ == "__main__":
+    app.run(debug=True)
