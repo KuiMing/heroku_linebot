a3fe32d25f7cbbbf977ad501b0d94a28be228e6c
KuiMing <benjamin0901@gmail.com>
2022-04-10 21:33:27 +0800

    remove unused

--- application.py
+++ application.py
@@ -1,13 +1,16 @@
+"""
+Line Chatbot tutorial
+"""
 from flask import Flask
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@APP.route("/")
+def hello() -> str:
     "hello world"
     return "Hello World!!!!!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    APP.run(debug=True)
