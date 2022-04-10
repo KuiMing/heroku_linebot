08aef86064d5a8892a7b51af1c29b22502742b9b
KuiMing <benjamin0901@gmail.com>
2022-04-10 22:22:46 +0800

    remove unused for flex message

--- application.py
+++ application.py
@@ -1,27 +1,34 @@
+"""
+Line Chatbot tutorial
+"""
 import os
 import json
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage,
-                            FlexSendMessage)
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
+    FlexSendMessage,
+)
 import investpy
 
-app = Flask(__name__)
+APP = Flask(__name__)
 LINE_SECRET = os.getenv('LINE_SECRET')
 LINE_TOKEN = os.getenv('LINE_TOKEN')
 LINE_BOT = LineBotApi(LINE_TOKEN)
 HANDLER = WebhookHandler(LINE_SECRET)
 
 
-@app.route("/")
-def hello():
+@APP.route("/")
+def hello() -> str:
     "hello world"
     return "Hello World!!!!!"
 
 
-@app.route("/callback", methods=["POST"])
-def callback():
+@APP.route("/callback", methods=["POST"])
+def callback() -> str:
     """
     LINE bot webhook callback
     """
@@ -40,7 +47,10 @@ def callback():
     return "OK"
 
 
-def bubble_currency():
+def bubble_currency() -> dict:
+    """
+    create currency bubble
+    """
     with open("bubble.json", "r") as f_h:
         bubble = json.load(f_h)
     f_h.close()
@@ -52,25 +62,22 @@ def bubble_currency():
 
 
 @HANDLER.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def handle_message(event: MessageEvent) -> None:
     """
     Reply text message
     """
-    with open("bubble.json", "r") as f_h:
-        bubble = json.load(f_h)
-    f_h.close()
     text = event.message.text.replace(" ", "").lower()
-    if text == "github":
-        output = "https://github.com/KuiMing/heroku_linebot"
-        message = TextSendMessage(text=output)
-    elif text == "currency":
+    if text == "currency":
         bubble = bubble_currency()
         message = FlexSendMessage(alt_text="Report", contents=bubble)
+        LINE_BOT.reply_message(event.reply_token, message)
+    if text == "github":
+        output = "https://github.com/KuiMing/heroku_linebot"
     else:
         output = text
-        message = TextSendMessage(text=output)
+    message = TextSendMessage(text=output)
     LINE_BOT.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    APP.run(debug=True)
